09800af91e90a5048866970ef4ca2dce20020653
Leonardo_13 <leonardo.novak5@gmail.com>
2023-09-15 11:14:05 +0200

    API response successful - popular movies list received and displayed

--- app/src/main/java/com/example/thirdmodule/ApiMovie.kt
+++ app/src/main/java/com/example/thirdmodule/ApiMovie.kt
@@ -1,4 +1,51 @@
 package com.example.thirdmodule
 
-class ApiMovie {
-}
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+
+class ApiMovie(
+    @SerializedName("adult")
+    @Expose
+    val adult: Boolean,
+    @SerializedName("backdrop_path")
+    @Expose
+    val backdropPath: String,
+    @SerializedName("id")
+    @Expose
+    val id: Int,
+    @SerializedName("title")
+    @Expose
+    val title: String,
+    @SerializedName("poster_path")
+    @Expose
+    val imagePath: String,
+    @SerializedName("release_date")
+    @Expose
+    val releaseDate: String,
+    @SerializedName("overview")
+    @Expose
+    val overview: String,
+    @SerializedName("vote_average")
+    @Expose
+    val voteAverage: Number,
+    @SerializedName("vote_count")
+    @Expose
+    val voteCount: Int,
+    @SerializedName("popularity")
+    @Expose
+    val popularity: Number,
+    @SerializedName("video")
+    @Expose
+    val video: Boolean,
+    @SerializedName("original_language")
+    @Expose
+    val originalLanguage: String,
+    @SerializedName("original_title")
+    @Expose
+    val originalTitle: String,
+    @SerializedName("genre_ids")
+    @Expose
+    val genreIds: List<Int>,
+
+
+)
--- app/src/main/java/com/example/thirdmodule/ApiMovieResponse.kt
+++ app/src/main/java/com/example/thirdmodule/ApiMovieResponse.kt
@@ -1,4 +1,14 @@
 package com.example.thirdmodule
 
-class ApiMovieResponse {
-}
+import com.google.gson.annotations.SerializedName
+
+data class ApiMovieResponse(
+    @SerializedName("page")
+    val page: Int,
+    @SerializedName("results")
+    val results: List<ApiMovie>,
+    @SerializedName("total_pages")
+    val totalPages: Int,
+    @SerializedName("total_results")
+    val totalResults: Int
+)
